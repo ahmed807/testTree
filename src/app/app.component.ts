1277668ffa7c78aec4ab5d7b8b8fd1d7c51b2a70
ahmed807 <ahmedcicsap@outlook.com>
2024-03-31 22:43:40 +0200

    new tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { NodeService } from '../../service/nodeservice';
 import { RouterOutlet } from '@angular/router';
+import { TreeNode } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'testTree';
+export class AppComponent implements OnInit {
+  files!: TreeNode[];
+
+  constructor(private nodeService: NodeService) {}
+
+  ngOnInit() {
+    this.nodeService.getFilesystem().then((files) => (this.files = files));
+  }
 }
