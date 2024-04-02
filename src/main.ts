86cf09504dfde9b65797fc8abee4a64c4f200890
ahmed807 <ahmedcicsap@outlook.com>
2024-04-03 00:17:37 +0200

    multi select worked after adding  provideAnimations in app.config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { AppComponent } from './app/app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule here
 import { appConfig } from './app/app.config';
 import { bootstrapApplication } from '@angular/platform-browser';
 
